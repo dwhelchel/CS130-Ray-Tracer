8ddb53c740b818184e5a2a9307a00e51565820a6
Dakota Whelchel <dwhel001@ucr.edu>
2019-01-29 17:07:35 -0800

    testing

--- proj-rt/proj-rt-files/phong_shader.cpp
+++ proj-rt/proj-rt-files/phong_shader.cpp
@@ -11,12 +11,12 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     vec3 color;
     TODO; //determine the color
     vec3 ambient_component = color_ambient * world.ambient_color * world.ambient_intensity;
-    vec3 specular_component, diffuse_component;
+    vec3 specular_component, diffuse_component = {0, 0, 0};
     color += ambient_component;
     for (unsigned int i = 0; i < world.lights.size(); ++i) {
         Ray shadowRay = Ray(world.lights[i]->position, world.lights[i]->position - intersection_point);
         // do if statement here
-        diffuse_component = color_diffuse * world.lights[i]->Emitted_Light(shadowRay.direction) * std::max(dot(normal, shadowRay.direction), 0.0);
+        // diffuse_component = color_diffuse * world.lights[i]->Emitted_Light(shadowRay.direction) * std::max(dot(normal, shadowRay.direction), 0.0);
         // specular_component = color_specular * world.lights[i].Emitted_Light() * max(, 0);
         color += diffuse_component;
         color += specular_component;
