2b4f5b54987e56c148c02f4f9726ee2a3f2a1aef
Dakota <dakotabwhelchel@gmail.com>
2019-02-05 03:35:07 -0800

    added reflective shader

--- proj-rt/proj-rt-files/reflective_shader.cpp
+++ proj-rt/proj-rt-files/reflective_shader.cpp
@@ -8,8 +8,12 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     TODO; // determine the color
-    // vec3 view = ray.endpoint - intersection_point;
+    vec3 vDir = ray.direction - intersection_point;
+    vec3 rDir = 2*dot(vDir, normal)*normal - vDir;
+    vec3 sColor = shader->Shade_Surface(ray, intersection_point, normal, recursion_depth);
 
-    // color = (1-reflectivity)*color+reflectivity*world.Cast_Ray(reflected_ray, recursion_depth);
+    Ray rRay = {intersection_point, rDir};
+
+    color = (1-reflectivity) * sColor + reflectivity * world.Cast_Ray(rRay, recursion_depth);
     return color;
 }
