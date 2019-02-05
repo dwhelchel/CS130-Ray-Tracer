202caaa7f1bc46007c1f07a45ac2d59e50e09691
Dakota <dakotabwhelchel@gmail.com>
2019-02-05 03:44:12 -0800

    fixing issues

--- proj-rt/proj-rt-files/reflective_shader.cpp
+++ proj-rt/proj-rt-files/reflective_shader.cpp
@@ -8,12 +8,16 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     TODO; // determine the color
-    vec3 vDir = ray.direction - intersection_point;
-    vec3 rDir = 2*dot(vDir, normal)*normal - vDir;
+
+    Ray reflection_ray;
+    vec3 vDir = ray.endpoint - intersection_point;
+    vec3 rDir = 2*dot(rDir, normal)*normal - rDir;
+    reflection_ray = {intersection_point, rDir};
+
+    vec3 rColor = world.Cast_Ray(reflection_ray, recursion_depth);
     vec3 sColor = shader->Shade_Surface(ray, intersection_point, normal, recursion_depth);
 
-    Ray rRay = {intersection_point, rDir};
+    color = (1-reflectivity) * sColor + reflectivity * rColor;
 
-    color = (1-reflectivity) * sColor + reflectivity * world.Cast_Ray(rRay, recursion_depth);
     return color;
 }
